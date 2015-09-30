d17b6febb372a5ebc7ff5d40971904d0b577b226
LauraKirby <laurakirby26@gmail.com>
2015-09-30 12:05:21 -0700

    set up request to HealthyMe API

--- HealthyMe/RaceTableViewController.m
+++ HealthyMe/RaceTableViewController.m
@@ -17,6 +17,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    //connect to HealthyMe API, a Rails app route which renders JSON
+    NSURL *raceURL = [NSURL URLWithString:@"http://localhost:3000/races.json"];
+
+    NSData *jsonRaceData = [NSData dataWithContentsOfURL:raceURL];
+
+    NSError *error = nil;
+
+    NSDictionary *raceDataDictionary = [NSJSONSerialization JSONObjectWithData:jsonRaceData options:0 error:&error];
+
+    //add data to our RaceTableViewController property
+    self.allRaces = [NSMutableArray array];
+
+    //each race object is a dictionary. we have an array of dictionaries
+    NSArray *raceArray = [raceDataDictionary objectForKey:@"races"];
     
 }
 
